7e98dd47e03f8a5a10ddc984f6f438c70ffbec97
William Henderson <william-henderson@outlook.com>
2021-09-05 17:20:05 +0100

    Updated example plugin to override the "/override" route

--- examples/plugin/src/lib.rs
+++ examples/plugin/src/lib.rs
@@ -1,5 +1,5 @@
 use humphrey::http::headers::ResponseHeader;
-use humphrey::http::{Request, Response};
+use humphrey::http::{Request, Response, StatusCode};
 
 use humphrey_server::declare_plugin;
 use humphrey_server::plugins::plugin::Plugin;
@@ -15,13 +15,26 @@ impl Plugin for ExamplePlugin {
         "Example Plugin"
     }
 
-    fn on_load(&mut self) {}
-
-    fn on_request(&mut self, request: &mut Request, state: Arc<AppState>) {
+    fn on_request(&mut self, request: &mut Request, state: Arc<AppState>) -> Option<Response> {
         state.logger.info(&format!(
             "Example plugin read a request from {}",
             request.address
         ));
+
+        // If the requested resource is "/override" then override the response (which would ordinarily be 404).
+        if &request.uri == "/override" {
+            state.logger.info("Example plugin overrode a response");
+
+            return Some(
+                Response::new(StatusCode::OK)
+                    .with_bytes(b"Response overridden by example plugin :)".to_vec())
+                    .with_header(ResponseHeader::ContentType, "text/plain".into())
+                    .with_request_compatibility(request)
+                    .with_generated_headers(),
+            );
+        }
+
+        None
     }
 
     fn on_response(&mut self, response: &mut Response, state: Arc<AppState>) {
@@ -37,8 +50,6 @@ impl Plugin for ExamplePlugin {
             .logger
             .info("Example plugin added the X-Example-Plugin header to a response");
     }
-
-    fn on_unload(&mut self) {}
 }
 
 // Declare the plugin
