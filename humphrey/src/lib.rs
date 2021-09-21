f0efddc4afa1c1fe66a0a1b363a417e96ec81282
William Henderson <william-henderson@outlook.com>
2021-09-21 19:11:30 +0100

    Prepared `humphrey` crate for first publish

--- humphrey/src/lib.rs
+++ humphrey/src/lib.rs
@@ -1,4 +1,6 @@
-//! # Humphrey: A Performance-Focused, Lightweight Web Server.
+//! # Humphrey: A Performance-Focused, Dependency-Free Web Server.
+//!
+//! Humphrey is a very fast, robust and flexible HTTP/1.1 web server, with support for static and dynamic content through its Rust crate and plugin system. It has no dependencies when only using default features, and is easily extensible with a configuration file, dynamically-loaded plugins, or its Rust crate.
 
 pub mod app;
 pub mod http;
