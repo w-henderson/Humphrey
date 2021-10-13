7dd5a36b586c4c950e9cd41a65b52e0aae335a56
William Henderson <william-henderson@outlook.com>
2021-10-13 11:32:16 +0100

    Updated path finding to better handle directories

--- humphrey-server/src/server/route.rs
+++ humphrey-server/src/server/route.rs
@@ -1,15 +1,19 @@
 use std::fs::metadata;
 use std::path::PathBuf;
 
-pub struct LocatedPath {
-    pub path: PathBuf,
-    pub was_redirected: bool,
+pub enum LocatedPath {
+    Directory,
+    File(PathBuf),
 }
 
 /// Attemps to find a given path.
 /// If the path itself is not found, attemps to find index files within it.
 /// If these are not found, returns `None`.
-pub fn try_find_path(directory: &str, request_path: &str, php: bool) -> Option<LocatedPath> {
+pub fn try_find_path(
+    directory: &str,
+    request_path: &str,
+    index_files: &[&str],
+) -> Option<LocatedPath> {
     if request_path.contains("..") || request_path.contains(':') {
         return None;
     }
@@ -17,27 +21,27 @@ pub fn try_find_path(directory: &str, request_path: &str, php: bool) -> Option<L
     let request_path = request_path.trim_start_matches('/');
     let directory = directory.trim_end_matches('/');
 
-    let paths: Vec<String> = if php {
-        if request_path.ends_with(".php") {
-            vec![format!("{}/{}", directory, request_path)]
-        } else {
-            vec![format!("{}/{}/index.php", directory, request_path)]
+    if request_path.ends_with('/') || request_path.is_empty() {
+        for filename in index_files {
+            let path = format!("{}/{}{}", directory, request_path, *filename);
+            if let Ok(meta) = metadata(&path) {
+                if meta.is_file() {
+                    return Some(LocatedPath::File(
+                        PathBuf::from(path).canonicalize().unwrap(),
+                    ));
+                }
+            }
         }
     } else {
-        vec![
-            format!("{}/{}", directory, request_path),
-            format!("{}/{}/index.html", directory, request_path),
-            format!("{}/{}/index.htm", directory, request_path),
-        ]
-    };
+        let path = format!("{}/{}", directory, request_path);
 
-    for (index, path) in paths.iter().enumerate() {
-        if let Ok(meta) = metadata(path) {
+        if let Ok(meta) = metadata(&path) {
             if meta.is_file() {
-                return Some(LocatedPath {
-                    path: PathBuf::from(path).canonicalize().unwrap(),
-                    was_redirected: index != 0,
-                });
+                return Some(LocatedPath::File(
+                    PathBuf::from(path).canonicalize().unwrap(),
+                ));
+            } else if meta.is_dir() {
+                return Some(LocatedPath::Directory);
             }
         }
     }
--- humphrey-server/src/server/static.rs
+++ humphrey-server/src/server/static.rs
@@ -1,4 +1,4 @@
-use crate::route::try_find_path;
+use crate::route::{try_find_path, LocatedPath};
 use crate::server::server::AppState;
 
 use humphrey::http::headers::ResponseHeader;
@@ -8,6 +8,8 @@ use humphrey::http::{Request, Response, StatusCode};
 use std::fs::File;
 use std::io::Read;
 use std::sync::Arc;
+
+const INDEX_FILES: [&str; 2] = ["index.html", "index.htm"];
 
 #[cfg(feature = "plugins")]
 pub fn file_handler(mut request: Request, state: Arc<AppState>, directory: &str) -> Response {
@@ -65,48 +67,47 @@ fn inner_file_handler(request: Request, state: Arc<AppState>, directory: &str) -
         drop(cache);
     }
 
-    if let Some(located) = try_find_path(directory, &request.uri, false) {
-        if located.was_redirected && !request.uri.ends_with('/') && !request.uri.is_empty() {
-            state.logger.info(&format!(
-                "{}: 301 Moved Permanently {}",
-                request.address, request.uri
-            ));
-            return Response::new(StatusCode::MovedPermanently)
-                .with_header(ResponseHeader::Location, format!("{}/", &request.uri))
-                .with_request_compatibility(&request)
-                .with_generated_headers();
+    if let Some(located) = try_find_path(directory, &request.uri, &INDEX_FILES) {
+        match located {
+            LocatedPath::Directory => {
+                state.logger.info(&format!(
+                    "{}: 301 Moved Permanently {}",
+                    request.address, request.uri
+                ));
+                Response::new(StatusCode::MovedPermanently)
+                    .with_header(ResponseHeader::Location, format!("{}/", &request.uri))
+                    .with_request_compatibility(&request)
+                    .with_generated_headers()
+            }
+            LocatedPath::File(path) => {
+                let file_extension = path.extension().map(|s| s.to_str().unwrap()).unwrap_or("");
+
+                let mime_type = MimeType::from_extension(file_extension);
+                let mut contents: Vec<u8> = Vec::new();
+
+                let mut file = File::open(path).unwrap();
+                file.read_to_end(&mut contents).unwrap();
+
+                if state.config.cache.size_limit >= contents.len() {
+                    let mut cache = state.cache.write().unwrap();
+                    cache.set(&request.uri, contents.clone(), mime_type);
+                    state.logger.debug(&format!("Cached route {}", request.uri));
+                } else if state.config.cache.size_limit > 0 {
+                    state
+                        .logger
+                        .warn(&format!("Couldn't cache, cache too small {}", request.uri));
+                }
+
+                state
+                    .logger
+                    .info(&format!("{}: 200 OK {}", request.address, request.uri));
+                Response::new(StatusCode::OK)
+                    .with_header(ResponseHeader::ContentType, mime_type.into())
+                    .with_bytes(contents)
+                    .with_request_compatibility(&request)
+                    .with_generated_headers()
+            }
         }
-
-        let file_extension = located
-            .path
-            .extension()
-            .map(|s| s.to_str().unwrap())
-            .unwrap_or("");
-
-        let mime_type = MimeType::from_extension(file_extension);
-        let mut contents: Vec<u8> = Vec::new();
-
-        let mut file = File::open(located.path).unwrap();
-        file.read_to_end(&mut contents).unwrap();
-
-        if state.config.cache.size_limit >= contents.len() {
-            let mut cache = state.cache.write().unwrap();
-            cache.set(&request.uri, contents.clone(), mime_type);
-            state.logger.debug(&format!("Cached route {}", request.uri));
-        } else if state.config.cache.size_limit > 0 {
-            state
-                .logger
-                .warn(&format!("Couldn't cache, cache too small {}", request.uri));
-        }
-
-        state
-            .logger
-            .info(&format!("{}: 200 OK {}", request.address, request.uri));
-        Response::new(StatusCode::OK)
-            .with_header(ResponseHeader::ContentType, mime_type.into())
-            .with_bytes(contents)
-            .with_request_compatibility(&request)
-            .with_generated_headers()
     } else {
         state.logger.warn(&format!(
             "{}: 404 Not Found {}",
