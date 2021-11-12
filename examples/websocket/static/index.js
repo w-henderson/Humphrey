457c3023a37c826e572d212a5eab8d808f84413d
William Henderson <william-henderson@outlook.com>
2021-11-12 20:02:05 +0000

    Added WebSocket example

---
+++ examples/websocket/static/index.js
@@ -0,0 +1,25 @@
+const ws = new WebSocket(`ws://${window.location.hostname}`);
+
+function write(message) {
+  document.querySelector("div").innerText += `[server]  ${message}`;
+  document.querySelector("div").innerHTML += "<br>";
+}
+
+ws.onmessage = (e) => write(e.data);
+ws.onopen = () => write("<connected>");
+ws.onclose = () => write("<disconnected>");
+ws.onerror = () => write("<error>");
+
+function sendMessage() {
+  let data = document.querySelector("#message").value;
+  document.querySelector("div").innerText += `[client]  ${data}`;
+  document.querySelector("div").innerHTML += "<br>";
+  ws.send(data);
+  document.querySelector("#message").value = "";
+}
+
+document.onkeydown = (e) => {
+  if (e.key === "Enter" && document.querySelector("#message").value !== "") {
+    sendMessage();
+  }
+}
