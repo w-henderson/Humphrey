2c1de26e6e4c67db77f23bf14d28ba122d9dc73f
William Henderson <william-henderson@outlook.com>
2021-12-21 16:38:42 +0000

    Fixed tests for new API

--- humphrey/src/tests/response.rs
+++ humphrey/src/tests/response.rs
@@ -29,11 +29,6 @@ fn test_response() {
         "text/html"
     );
 
-    assert_eq!(
-        response.get_headers().get(&ResponseHeader::Server).unwrap(),
-        "Humphrey"
-    );
-
     let expected_bytes: Vec<u8> = b"HTTP/1.1 200 OK\r\nDate: Thu, 1 Jan 1970 00:00:00 GMT\r\nContent-Language: en-GB\r\nContent-Type: text/html\r\n\r\n<body>test</body>\r\n".to_vec();
     let bytes: Vec<u8> = response.into();
 
