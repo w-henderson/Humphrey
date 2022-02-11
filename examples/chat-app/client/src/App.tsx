a8bfe3147a62ef5065b80ab6b73e85c81f47e13d
William Henderson <william-henderson@outlook.com>
2022-02-11 08:12:09 +0000

    Initialised client for chat app example

---
+++ examples/chat-app/client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./styles/App.scss";
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App"></div>
+    )
+  }
+}
+
+export default App;
