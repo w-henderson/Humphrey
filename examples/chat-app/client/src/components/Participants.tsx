94acdc82f92af12153c305586c53d50cbc2ec43b
William Henderson <william-henderson@outlook.com>
2022-02-14 16:29:40 +0000

    Added "about" information to example chat app

--- examples/chat-app/client/src/components/Participants.tsx
+++ examples/chat-app/client/src/components/Participants.tsx
@@ -10,6 +10,9 @@ export class Participants extends Component<ParticipantsProps> {
     if (this.props.participants === null || this.props.participants.length === 0) {
       return (
         <div className="Participants">
+          <h2>Humphrey Chat</h2>
+          Welcome to Humphrey Chat, an example real-time chat application built with Humphrey and Humphrey WebSocket. The backend is written in Rust using Humphrey and no other dependencies, and the frontend is written in TypeScript using React. You can find the source code for Humphrey Chat on <a href="https://github.com/w-henderson/Humphrey/tree/master/examples/chat-app" target="_blank">GitHub</a>.
+
           <h2>Participants</h2>
 
           Loading...
@@ -18,6 +21,9 @@ export class Participants extends Component<ParticipantsProps> {
     } else {
       return (
         <div className="Participants">
+          <h2>Humphrey Chat</h2>
+          Welcome to Humphrey Chat, an example real-time chat application built with Humphrey and Humphrey WebSocket. The backend is written in Rust using Humphrey and no other dependencies, and the frontend is written in TypeScript using React. You can find the source code for Humphrey Chat on <a href="https://github.com/w-henderson/Humphrey/tree/master/examples/chat-app" target="_blank">GitHub</a>.
+
           <h2>Participants</h2>
 
           <div className="container">
