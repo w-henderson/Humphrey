343892f75d9f956c5716bcba11cad71a43012122
William Henderson <william-henderson@outlook.com>
2022-02-22 14:47:59 +0000

    Added a number of tests for JSON macros

---
+++ humphrey-json/src/tests/macros.rs
@@ -0,0 +1,100 @@
+use crate::{json, Value};
+
+use super::hashmap_helper::to_object;
+
+#[test]
+fn test_macro_object() {
+    let value = json!({
+        "name": "William Henderson",
+        "favouriteNumber": 1.414,
+        "languages": [
+            "Rust",
+            "TypeScript",
+            1.234E3,
+            false,
+            {
+                "name": "Python",
+                "version": 3.6
+            }
+        ],
+        "weaknesses": [],
+        "funnyName": {},
+        "online": true,
+        "life": null
+    });
+
+    let expected_string = include_str!("./testcases/object.json");
+    let expected_value = Value::parse(expected_string).unwrap();
+
+    assert_eq!(value, expected_value);
+}
+
+#[test]
+fn test_macro_array() {
+    let value = json!([
+      {
+        "name": "w-henderson",
+        "favouriteNumber": 1.414,
+        "online": true
+      },
+      {
+        "name": "flauntingspade4",
+        "favouriteNumber": 69,
+        "online": false
+      }
+    ]);
+
+    let expected_string = include_str!("./testcases/array.json");
+    let expected_value = Value::parse(expected_string).unwrap();
+
+    assert_eq!(value, expected_value);
+}
+
+#[test]
+fn test_macro_string() {
+    let value = json!("Hello, world!");
+    let expected_value = Value::String("Hello, world!".into());
+
+    assert_eq!(value, expected_value);
+}
+
+#[test]
+fn test_macro_literals() {
+    let values = [json!(1234), json!(true), json!(false), json!(null), json!()];
+
+    let expected_values = [
+        Value::Number(1234.0),
+        Value::Bool(true),
+        Value::Bool(false),
+        Value::Null,
+        Value::Null,
+    ];
+
+    for (value, expected_value) in values.iter().zip(expected_values.iter()) {
+        assert_eq!(*value, *expected_value);
+    }
+}
+
+#[test]
+fn test_macro_embedded_variables() {
+    let embedded_string = "Hello, world!";
+    let embedded_number: u16 = 1234;
+    let embedded_some = Some("value");
+    let embedded_none: Option<&str> = None;
+
+    let value = json!({
+        "string": embedded_string,
+        "number": embedded_number,
+        "some": embedded_some,
+        "none": embedded_none
+    });
+
+    let expected_value = Value::Object(to_object(vec![
+        ("string", Value::String("Hello, world!".into())),
+        ("number", Value::Number(1234.0)),
+        ("some", Value::String("value".into())),
+        ("none", Value::Null),
+    ]));
+
+    assert_eq!(value, expected_value);
+}
--- humphrey-json/src/tests/mod.rs
+++ humphrey-json/src/tests/mod.rs
@@ -1,4 +1,5 @@
 pub mod indexing;
+pub mod macros;
 pub mod parse_invalid;
 pub mod parse_valid;
 
