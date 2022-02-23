ce4691d8de940d00034b25b690e89abcb3155773
William Henderson <william-henderson@outlook.com>
2022-02-23 09:20:41 +0000

    Added `warn(missing_docs)` to JSON crate

--- humphrey-json/src/lib.rs
+++ humphrey-json/src/lib.rs
@@ -1,3 +1,9 @@
+//! Humphrey JSON is a library facilitating the serialization and deserialization of JSON data. It is designed for web applications, but can be used in other contexts, and is well-integrated with the Humphrey web server.
+//!
+//! Learn more about Humphrey JSON [here (coming soon)](https://humphrey.whenderson.dev/json/index.html).
+
+#![warn(missing_docs)]
+
 pub mod error;
 pub mod parser;
 pub mod traits;
@@ -9,6 +15,11 @@ pub mod macros;
 #[cfg(test)]
 mod tests;
 
+/// Brings useful traits into scope.
+///
+/// ```rs
+/// use humphrey_json::prelude::*;
+/// ```
 pub mod prelude {
     pub use crate::traits::*;
 }
