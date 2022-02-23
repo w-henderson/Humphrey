0909f9da8ce9d0bc2e9ff90e92a7032876a12683
William Henderson <william-henderson@outlook.com>
2022-02-23 09:33:51 +0000

    Documented JSON traits and methods

--- humphrey-json/src/traits.rs
+++ humphrey-json/src/traits.rs
@@ -1,12 +1,20 @@
+//! Provides useful traits for working with JSON values.
+
 use crate::Value;
 
 use std::error::Error;
 
+/// Represents the ability of a type to be converted into a JSON value.
+///
+/// This trait is implemented for both core string types, the boolean type, all numeric types, and all `Option<T>` where `T` implements the trait.
 pub trait IntoJson {
+    /// Creates a JSON value from itself.
     fn to_json(&self) -> Value;
 }
 
+/// Represents the ability of a type to be constructed from a JSON value.
 pub trait FromJson {
+    /// Constructs itself from a JSON value.
     fn from_json(value: &Value) -> Result<Self, Box<dyn Error>>
     where
         Self: Sized;
