bbef4dc4d92656658152674f0506d7dab6733368
William Henderson <william-henderson@outlook.com>
2022-02-23 09:27:37 +0000

    Documented JSON value type and methods

--- humphrey-json/src/value.rs
+++ humphrey-json/src/value.rs
@@ -1,17 +1,45 @@
+//! Provides the `Value` struct for interfacing with JSON values.
+
 use std::collections::HashMap;
 use std::ops::Index;
 
+/// Represents a JSON value.
+///
+/// ## Constructing
+/// The `true` value, for example, can be constructed as any of the following:
+/// ```rs
+/// let _ = Value::Bool(true);
+/// let _ = json!(true);
+/// let _ = Value::parse("true");
+/// ```
 #[derive(Clone, Debug)]
 pub enum Value {
+    /// The `null` value.
     Null,
+    /// A boolean value, `true` or `false`.
+    ///
+    /// Can be extracted with `as_bool()`.
     Bool(bool),
+    /// A numeric value, stored as a float in accordance with the specification.
+    ///
+    /// Can be extracted with `as_number()`.
     Number(f64),
+    /// A UTF-8 string value.
+    ///
+    /// Can be extracted with `as_str()`.
     String(String),
+    /// An array of values.
+    ///
+    /// Can be extracted with `as_array()`.
     Array(Vec<Value>),
+    /// An object mapping of values.
+    ///
+    /// Can be extracted with `as_map()`.
     Object(HashMap<String, Value>),
 }
 
 impl Value {
+    /// Returns the encapsulated boolean value, or `None` if it is not a boolean data type.
     pub fn as_bool(&self) -> Option<bool> {
         match self {
             Value::Bool(b) => Some(*b),
@@ -19,6 +47,7 @@ impl Value {
         }
     }
 
+    /// Returns the encapsulated numeric value, or `None` if it is not a numeric data type.
     pub fn as_number(&self) -> Option<f64> {
         match self {
             Value::Number(n) => Some(*n),
@@ -26,6 +55,7 @@ impl Value {
         }
     }
 
+    /// Returns the encapsulated string value, or `None` if it is not a string data type.
     pub fn as_str(&self) -> Option<&str> {
         match self {
             Value::String(s) => Some(s.as_str()),
@@ -33,6 +63,7 @@ impl Value {
         }
     }
 
+    /// Returns the encapsulated array value, or `None` if it not an array data type.
     pub fn as_array(&self) -> Option<&Vec<Value>> {
         match self {
             Value::Array(a) => Some(a),
@@ -40,6 +71,7 @@ impl Value {
         }
     }
 
+    /// Returns the encapsulated hash map value, or `None` if it not an object data type.
     pub fn as_map(&self) -> Option<&HashMap<String, Value>> {
         match self {
             Value::Object(o) => Some(o),
