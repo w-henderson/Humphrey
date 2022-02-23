dca7be4d9fb5fc869864951c536f3c26dd9def3d
William Henderson <william-henderson@outlook.com>
2022-02-23 10:04:55 +0000

    Documented `json!` macro

--- humphrey-json/src/macros.rs
+++ humphrey-json/src/macros.rs
@@ -1,3 +1,26 @@
+//! Provides the `json!` macro for creating JSON values.
+
+/// Create a JSON value from JSON-like syntax.
+///
+/// ## Usage
+/// The macro allows you to create any kind of JSON value.
+///
+/// ```rs
+/// let value = json!({
+///     "key": "value",
+///     "array": [1, 2, 3],
+///     "empty": null,
+///     "bool": true
+/// });
+/// ```
+///
+/// It can also be used as a shorthand to create literal values.
+///
+/// ```rs
+/// assert_eq!(json!(true), Value::Bool(true));
+/// assert_eq!(json!(1234), Value::Number(1234.0));
+/// assert_eq!(json!("Hello, world!"), Value::String("Hello, world!".into()));
+/// ```
 #[macro_export]
 macro_rules! json {
     () => {
