d6ade1176a1988a7ec74b86b7593b8eecc727413
William Henderson <william-henderson@outlook.com>
2022-02-26 19:12:57 +0000

    Updated chat app front-end to use JSON

--- examples/chat-app/client/src/App.tsx
+++ examples/chat-app/client/src/App.tsx
@@ -21,6 +21,32 @@ enum AppPhase {
   Connected
 }
 
+enum ClientMessageKind {
+  Register,
+  Chat
+}
+
+enum ServerMessageKind {
+  Id,
+  Participants,
+  Join,
+  Chat,
+  Leave
+}
+
+interface ClientMessage {
+  kind: ClientMessageKind,
+  message: string
+}
+
+interface ServerMessage {
+  kind: ServerMessageKind,
+  message: string | null,
+  senderId: number,
+  senderName: string | null,
+  participants?: string[]
+}
+
 export class App extends Component<{}, AppState> {
   ws: WebSocket | null;
 
@@ -39,8 +65,6 @@ export class App extends Component<{}, AppState> {
     this.connect = this.connect.bind(this);
     this.recvId = this.recvId.bind(this);
     this.recvMessage = this.recvMessage.bind(this);
-    this.recvChatMessage = this.recvChatMessage.bind(this);
-    this.recvParticipantUpdateMessage = this.recvParticipantUpdateMessage.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
   }
 
@@ -54,72 +78,78 @@ export class App extends Component<{}, AppState> {
     this.ws.onmessage = e => this.recvId(e, username);
   }
 
-  async recvId(e: MessageEvent, username: string) {
-    let buf = await e.data.arrayBuffer();
-    let view = new DataView(buf, 4, 4);
-    let id = view.getUint32(0, false);
+  recvId(e: MessageEvent, username: string) {
+    let incomingMessage: ServerMessage = JSON.parse(e.data);
 
-    this.setState({ id, phase: AppPhase.Connected });
+    let message: ClientMessage = {
+      kind: ClientMessageKind.Register,
+      message: username
+    };
+
+    this.setState({ id: incomingMessage.senderId, phase: AppPhase.Connected });
     this.ws!.onmessage = this.recvMessage;
-    this.ws!.send(username);
+    this.ws!.send(JSON.stringify(message));
   }
 
-  async recvMessage(e: MessageEvent) {
-    let buf: ArrayBuffer = await e.data.arrayBuffer();
+  recvMessage(e: MessageEvent) {
+    let incomingMessage: ServerMessage = JSON.parse(e.data);
 
-    let typeView = new DataView(buf, 0, 1);
-    let messageType = typeView.getUint8(0);
+    switch (incomingMessage.kind) {
+      case ServerMessageKind.Participants: {
+        return this.setState({ participants: incomingMessage.participants! });
+      }
+      case ServerMessageKind.Join: {
+        let participants = this.state.participants;
+        let messages = this.state.messages;
 
-    if (messageType === 0) this.recvChatMessage(buf.slice(1));
-    else if (messageType === 1) this.recvParticipantUpdateMessage(buf.slice(1));
-  }
+        participants?.push(incomingMessage.senderName!);
 
-  recvChatMessage(buf: ArrayBuffer) {
-    let messageLengthView = new DataView(buf, 4, 4);
-    let messageLength = messageLengthView.getUint32(0, false);
-    let messageView = Array.from(new Uint8Array(buf, 8, messageLength));
-    let message = String.fromCharCode(...messageView);
+        messages.push({
+          message: `${incomingMessage.senderName!} has joined the chat`,
+          sender: "",
+          timestamp: new Date().getTime(),
+          type: MessageType.Broadcast
+        });
 
-    let senderIdView = new DataView(buf, 12 + messageLength, 4);
-    let senderId = senderIdView.getUint32(0, false);
+        return this.setState({ participants, messages });
+      }
+      case ServerMessageKind.Chat: {
+        let messages = this.state.messages;
 
-    let sender = "";
-    if (senderId !== 0) {
-      let senderLengthView = new DataView(buf, 20 + messageLength, 4);
-      let senderLength = senderLengthView.getUint32(0, false);
-      let senderView = Array.from(new Uint8Array(buf, 24 + messageLength, senderLength));
-      sender = String.fromCharCode(...senderView);
+        messages.push({
+          message: incomingMessage.message!,
+          sender: incomingMessage.senderName!,
+          timestamp: new Date().getTime(),
+          type: incomingMessage.senderId === this.state.id ? MessageType.Local : MessageType.Remote
+        });
+
+        return this.setState({ messages });
+      }
+      case ServerMessageKind.Leave: {
+        let participants = this.state.participants;
+        let messages = this.state.messages;
+
+        participants!.splice(participants!.indexOf(incomingMessage.senderName!), 1);
+
+        messages.push({
+          message: `${incomingMessage.senderName} has left the chat`,
+          sender: "",
+          timestamp: new Date().getTime(),
+          type: MessageType.Broadcast
+        });
+
+        return this.setState({ participants, messages });
+      }
     }
-
-    let messages = this.state.messages;
-    let timestamp = new Date().getTime();
-
-    let type = MessageType.Remote;
-    if (senderId === this.state.id) type = MessageType.Local;
-    if (senderId === 0) type = MessageType.Broadcast;
-
-    messages.push({
-      message,
-      sender,
-      timestamp,
-      type
-    });
-
-    this.setState({ messages });
-  }
-
-  recvParticipantUpdateMessage(buf: ArrayBuffer) {
-    let participantsLengthView = new DataView(buf, 4, 4);
-    let participantsLength = participantsLengthView.getUint32(0, false);
-
-    let participantsView = Array.from(new Uint8Array(buf, 8, participantsLength - 1));
-    let participants = String.fromCharCode(...participantsView).split("\n");
-
-    this.setState({ participants });
   }
 
   sendMessage(message: string) {
-    this.ws!.send(message);
+    let serialized: ClientMessage = {
+      kind: ClientMessageKind.Chat,
+      message
+    }
+
+    this.ws!.send(JSON.stringify(serialized));
   }
 
   render() {
